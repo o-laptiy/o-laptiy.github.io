bb76be6bc38a4f7da9daa12c552c94cb95b8395d
oleg <100022566+o-laptiy@users.noreply.github.com>
2022-04-27 21:17:20 +0600

    Update solve_quadratic.py

--- solve_quadratic.py
+++ solve_quadratic.py
@@ -4,8 +4,8 @@ def solve_quadratic(a, b, c):
    d = (b * b) - (4 * a * c)
    if (d < 0):
       return None
-   x1 = (-b + math.sqrt(d)) / 2 * a
-   x2 = (-b - math.sqrt(d)) / 2 * a
+   x1 = (-b + math.sqrt(d)) / (2 * a)
+   x2 = (-b - math.sqrt(d)) / (2 * a)
    if (d > 0):
       return (x1, x2)
    elif (d == 0):
